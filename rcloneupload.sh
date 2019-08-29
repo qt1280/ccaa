e2949174a8603bbacb45e6dbfe8746eaa01fe584
qt1280 <97007623@qq.com>
2019-08-29 13:27:06 +0800

    Update rcloneupload.sh

--- rcloneupload.sh
+++ rcloneupload.sh
@@ -2,7 +2,7 @@
 
 filepath=$3	 #取文件原始路径，如果是单文件则为/Download/a.mp4，如果是文件夹则该值为文件夹内第一个文件比如/Download/a/1.mp4
 path=${3%/*}	 #取文件根路径，如把/Download/a/1.mp4变成/Download/a
-downloadpath='/data/Download'	#Aria2下载目录
+downloadpath='/data/ccaaDown/'	#Aria2下载目录
 name='joe1280' #配置Rclone时的name
 folder='/alishare'	 #网盘里的文件夹，如果是根目录直接留空
 MinSize='10k'	 #限制最低上传大小，默认10k，BT下载时可防止上传其他无用文件。会删除文件，谨慎设置。
